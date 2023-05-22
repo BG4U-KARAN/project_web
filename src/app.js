522aaa7d3c483b8fb8c4be6a01990be766e62697
karan <110165652+BG4U-KARAN@users.noreply.github.com>
2023-05-22 10:32:32 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 require("colors");
 const connectDB = require("./database/connect");
 const insertDB = require("./database/insertDB");
-const forgetPass = require("./database/forget");
+// const forgetPass = require("./database/forget");
 
 const start = async () => {
     try {
